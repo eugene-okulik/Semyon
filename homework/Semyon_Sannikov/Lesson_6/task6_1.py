d97a131442979fef8eca3d529517b2b66edcf3ff
Dagotra <test1semen@yandex.ru>
2025-02-26 21:48:20 +0500

    created two tasks in Lesson_6

--- homework/Semyon_Sannikov/Lesson_6/task6_1.py
+++ homework/Semyon_Sannikov/Lesson_6/task6_1.py
@@ -1,9 +1,8 @@
-# Напишите программу, которая добавляет ‘ing’ в конец слов (к каждому слову) в тексте 
-# “Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero” 
-# и после этого выводит получившийся текст на экран. Знаки препинания не должны оказаться внутри слова. 
-# Если после слова идет запятая или точка, этот знак препинания должен идти после того же слова, но уже преобразованного.
-text = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
-words = text.split()
+text1 = 'Etiam tincidunt neque erat, quis molestie enim imperdiet vel.' 
+text2 = 'Integer urna nisl, facilisis vitae semper at, dignissim vitae libero'
+concatination = text1 + text2
+
+words = concatination.split()
 upgrade_text = []
 
 for word in words:
