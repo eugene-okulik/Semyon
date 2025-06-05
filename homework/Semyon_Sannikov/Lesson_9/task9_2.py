def07bd11de4281798292c3e09439dae694e8420
Dagotra <s.sannikov@turbocontract.ru>
2025-06-05 16:30:16 +0500

    Created tasks for formatting time and sorting temperature

---
+++ homework/Semyon_Sannikov/Lesson_9/task9_2.py
@@ -0,0 +1,14 @@
+temperatures = \
+    [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27,
+     22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
+
+def func(x):
+    return x > 28
+
+new_list = list(filter(func, temperatures))
+n = len(new_list)
+
+print(new_list)
+print(f' самая высокая температура {max(new_list)}'
+      f'\n самая низкая температура {min(new_list)}'
+      f'\n средняя температура {int(sum(new_list) / n)}')
