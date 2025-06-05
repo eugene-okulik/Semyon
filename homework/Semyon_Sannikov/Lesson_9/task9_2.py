303618ebd9306b09c443359daed2dc1fe247853d
Dagotra <s.sannikov@turbocontract.ru>
2025-06-05 16:33:54 +0500

    Created tasks for formatting time and sorting temperature

--- homework/Semyon_Sannikov/Lesson_9/task9_2.py
+++ homework/Semyon_Sannikov/Lesson_9/task9_2.py
@@ -2,8 +2,10 @@ temperatures = \
     [20, 15, 32, 34, 21, 19, 25, 27, 30, 32, 34, 30, 29, 25, 27,
      22, 22, 23, 25, 29, 29, 31, 33, 31, 30, 32, 30, 28, 24, 23]
 
+
 def func(x):
     return x > 28
+
 
 new_list = list(filter(func, temperatures))
 n = len(new_list)
